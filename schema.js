1187dcadfa787bb032b176d7cee330b261c7a18d
sbriggs-wtc <sbriggs@student.wethinkcode.co.za>
2020-09-10 20:59:10 +0200

    commit

--- schema.js
+++ schema.js
@@ -7,6 +7,7 @@ const{
     GraphQLList,
     GraphQLNonNull
 } = require('graphql');
+const { renderGraphiQL } = require('express-graphql/renderGraphiQL');
 
 //Hardcoded Data
 /* const customers = [
@@ -54,8 +55,53 @@ const RootQuery = new GraphQLObjectType({
         },
     }
 });
-// +- 07:00
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields:{
+        addCustomer:{
+            type: CustomerType,
+            args: {
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                email: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)},
+            },
+            resolve(parentValue, args){
+                return axios.post('http://localhost:3000/customers', {
+                    name: args.name,
+                    email: args.email,
+                    age: args.age
+                })
+                .then(res => res.data);
+            }
+        },
+        deleteCustomer:{
+            type: CustomerType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, args){
+                return axios.delete('http://localhost:3000/customers/' + args.id)
+                .then(res => res.data);
+            }
+        },
+        editCustomer:{
+            type: CustomerType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)},
+                name: {type: GraphQLString},
+                email: {type: GraphQLString},
+                age: {type: GraphQLInt},
+            },
+            resolve(parentValue, args){
+                return axios.patch('http://localhost:3000/customers/' + args.id, args)
+                .then(res => res.data);
+            }
+        },
+    }
+})
     
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
